46ba90731fa8e084aaff2a1c7902ce4e5edcf237
Marcell Purham <marcellpurham@gmail.com>
2019-10-29 18:45:45 -0500

    Added base code for geofence

--- GeoFence/ViewController.m
+++ GeoFence/ViewController.m
@@ -7,17 +7,41 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () {
+}
+
+@property (nonatomic) CLLocationCoordinate2D locationCoord2D;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) NSMutableArray *geofences;
 
 @end
 
 @implementation ViewController
 
+@synthesize locationCoord2D;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // Define geofence
+    self.locationCoord2D = CLLocationCoordinate2DMake(41.782910, -88.199455);
+    
+    // set radius
+    CLRegion *connector = [[CLCircularRegion alloc]
+                        initWithCenter:self.locationCoord2D
+                        radius:100.0
+                        identifier:@"Connector"];
+    
+    // Initialize Location Manager
+    self.locationManager = [[CLLocationManager alloc] init];
+    
+    // Begin monitoring our region
+    [self.locationManager startMonitoringForRegion:connector];
+        
 }
+
 
 
 @end
